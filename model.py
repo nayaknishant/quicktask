4295e15edfcebf7ef2ba94f6cefa3470abdeeb9a
nayaknishant <nn343@cornell.edu>
2021-02-04 17:24:41 -0500

    added in f1 score

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import f1_score
 
 f = open('data/train.txt', 'r')
 lines = f.readlines()
@@ -30,11 +31,13 @@ labels_to_binary = np.asarray([1 if label == 'H' else 0 for label in bleu_labels
 print(bleu_scores)
 print(labels_to_binary)
 
-x_train, x_val, y_train, y_val = train_test_split(bleu_scores, labels_to_binary, test_size=0.05, random_state=0)
+x_train, x_val, y_train, y_val = train_test_split(bleu_scores, labels_to_binary, test_size=0.2, random_state=0)
 print(x_train.shape, y_train.shape)
 print(x_val.shape, y_val.shape)
 clf = LogisticRegression(random_state=0).fit(x_train, y_train)
-print(clf.score(x_val, y_val))
-print(clf.score(x_train, y_train))
+y_pred = clf.predict(x_val)
+#print(clf.score(x_train, y_train))
+print(f1_score(y_val, y_pred, average='macro'))
+# TODO: Include the f-score calculation
 
 f.close()
