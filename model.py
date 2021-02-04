8e7bfba34619244ba2c9a578eba606e30d1a84ce
nayaknishant <nn343@cornell.edu>
2021-02-04 01:03:33 -0500

    working on tuning model

--- model.py
+++ model.py
@@ -1,4 +1,6 @@
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 
 f = open('data/train.txt', 'r')
 lines = f.readlines()
@@ -22,15 +24,17 @@ for line in lines:
     if is_float(line.strip()):
     	bleu_score = float(line.strip())
 bleu_lists = list(map(list, zip(*bleu_to_label)))
-bleu_scores = np.asarray(bleu_lists[0])
+bleu_scores = np.asarray(bleu_lists[0]).reshape(-1, 1)
 bleu_labels = bleu_lists[1]
 labels_to_binary = np.asarray([1 if label == 'H' else 0 for label in bleu_labels])
 print(bleu_scores)
 print(labels_to_binary)
 
-from sklearn.datasets import load_iris
-from sklearn.linear_model import LogisticRegression
-X, y = load_iris(return_X_y=True)
-print(type(X))
+x_train, x_val, y_train, y_val = train_test_split(bleu_scores, labels_to_binary, test_size=0.05, random_state=0)
+print(x_train.shape, y_train.shape)
+print(x_val.shape, y_val.shape)
+clf = LogisticRegression(random_state=0).fit(x_train, y_train)
+print(clf.score(x_val, y_val))
+print(clf.score(x_train, y_train))
 
 f.close()
