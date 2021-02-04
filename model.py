a6e032a628d1577607b245e97278d8ea59cf3039
nayaknishant <nn343@cornell.edu>
2021-02-04 00:48:17 -0500

    manipulatin data

--- model.py
+++ model.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 f = open('data/train.txt', 'r')
 lines = f.readlines()
 
@@ -8,15 +10,27 @@ def is_float(value):
   except:
     return False
 
-
 count = 0
+bleu_score = 0
+bleu_to_label = []
 # Strips the newline character
 for line in lines:
     count += 1
     print("Line{}: {}".format(count, line.strip()))
     if line.strip() == 'H' or line.strip() == 'M':
-    	print("AYO")
+    	bleu_to_label.append(tuple((bleu_score, line.strip())))
     if is_float(line.strip()):
-    	print(float(line.strip()))
+    	bleu_score = float(line.strip())
+bleu_lists = list(map(list, zip(*bleu_to_label)))
+bleu_scores = np.asarray(bleu_lists[0])
+bleu_labels = bleu_lists[1]
+labels_to_binary = np.asarray([1 if label == 'H' else 0 for label in bleu_labels])
+print(bleu_scores)
+print(labels_to_binary)
+
+from sklearn.datasets import load_iris
+from sklearn.linear_model import LogisticRegression
+X, y = load_iris(return_X_y=True)
+print(type(X))
 
 f.close()
